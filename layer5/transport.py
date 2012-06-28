2743017a3ef36db8311f583381cc8818931c3221
Tomer Filiba <tomerf@il.ibm.com>
2012-06-28 20:00:43 +0300

    layer5

--- layer5/transport.py
+++ layer5/transport.py
@@ -2,13 +2,17 @@ import socket
 from struct import Struct
 import itertools
 import json
+import zlib
+import ssl
 
 
-class Listener(object):
-    def __init__(self, port, host = "", backlog = 10, port = 0, family = 0, stype = 0, proto = 0):
+class ListenerSocket(object):
+    def __init__(self, port, host = "", backlog = 10, family = socket.AF_INET, 
+            stype = socket.SOCK_STREAM, proto = 0):
         self.sock = socket.socket(family, stype, proto)
         self.sock.bind((host, port))
         self.sock.listen(backlog)
+        self.host, self.port = self.sock.getsockname()
     def accept(self):
         sock2, _ = self.sock.accept()
         return StreamSocket(sock2)
@@ -29,11 +33,25 @@ class StreamSocket(object):
     MAX_CHUNK = 16384
     def __init__(self, sock):
         self.sock = sock
+    def __del__(self):
+        try:
+            self.close()
+        except Exception:
+            pass
+    
     @classmethod
-    def connect(cls, host, port, family = 0, stype = 0, proto = 0):
+    def connect(cls, host, port, family = socket.AF_INET, stype = socket.SOCK_STREAM, proto = 0):
         sock = socket.socket(family, stype, proto)
         sock.connect((host, port))
         return cls(sock)
+    
+    @classmethod
+    def connect_ssl(cls, host, port, family = socket.AF_INET, stype = socket.SOCK_STREAM, proto = 0,
+            keyfile = None, certfile = None, **ssl_options):
+        sock = socket.socket(family, stype, proto)
+        sock.connect((host, port))
+        sock2 = ssl.wrap_socket(sock, keyfile, certfile, server_side = False, **ssl_options)
+        return cls(sock2)
     
     def fileno(self):
         return self.sock.fileno()
@@ -47,7 +65,7 @@ class StreamSocket(object):
             self.sock.close()
             self.sock = ClosedSocket
     
-    def recv(self, count):
+    def read(self, count):
         data = []
         while count > 0:
             buf = self.sock.recv(min(count, self.MAX_CHUNK))
@@ -57,7 +75,7 @@ class StreamSocket(object):
             data.append(buf)
         return "".join(data)
     
-    def send(self, blob):
+    def write(self, blob):
         while blob:
             sent = self.sock.send(blob[:self.MAX_CHUNK])
             blob = blob[sent:]
@@ -76,6 +94,9 @@ class StreamMessageTransport(MessageTransport):
     def __init__(self, stream):
         self.stream = stream
         self.seq = itertools.count()
+    @classmethod
+    def connect(cls, *args, **kwargs):
+        return cls(StreamSocket.connect(*args, **kwargs))
     def close(self):
         self.stream.close()
     def send(self, blob, seq = None):
@@ -85,9 +106,9 @@ class StreamMessageTransport(MessageTransport):
         self.stream.write(header + blob)
         return seq
     def recv(self):
-        header = self.stream.recv(self.HEADER.size)
+        header = self.stream.read(self.HEADER.size)
         seq, length = self.HEADER.unpack(header)
-        blob = self.stream.recv(length)
+        blob = self.stream.read(length)
         return seq, blob
 
 
@@ -104,26 +125,30 @@ class Dispatcher(object):
     def __init__(self, transport, serializer, funcmap):
         self.transport = transport
         self.serializer = serializer
-        self.funcmap = {}
+        self.funcmap = funcmap
     def dispatch(self):
         seq, blob = self.transport.recv()
         try:
-            func, args = self.serializer.decode(blob)
+            funcname, args = self.serializer.decode(blob)
         except Exception as ex:
-            resp = (RESP_ERROR, "Could not decode blob")
+            resp = (RESP_ERROR, "Could not decode blob: %r" % (ex,))
         else:
-            if not isinstance(args, (tuple, list)):
-                resp = (RESP_ERROR, "Arguments must be a list or a tuple")
-            try:
-                res = func(*args)
-            except Exception as ex:
-                resp = (RESP_EXCEPTION, repr(ex))
+            if funcname not in self.funcmap:
+                resp = (RESP_ERROR, "Unknown function %r" % (funcname,))
+            elif not isinstance(args, (tuple, list)):
+                resp = (RESP_ERROR, "Arguments must be a list or a tuple, not %r" % (type(args),))
             else:
-                resp = (RESP_OK, res)
+                try:
+                    func = self.funcmap[funcname]
+                    res = func(*args)
+                except Exception as ex:
+                    resp = (RESP_EXCEPTION, repr(ex))
+                else:
+                    resp = (RESP_OK, res)
         try:
             blob2 = self.serializer.encode(resp)
-        except Exception:
-            resp = (RESP_ERROR, "Could not encode response")
+        except Exception as ex:
+            resp = (RESP_ERROR, "Could not encode response: %r" % (ex,))
             blob2 = self.serializer.encode(resp)
         self.transport.send(blob2, seq)
 
@@ -133,15 +158,16 @@ class Serializer(object):
     def decode(self, blob):
         raise NotImplementedError()
 
+class IdentitySerializer(Serializer):
+    encode = decode = staticmethod(lambda x: x)
+
 class JSONSerializer(Serializer):
     encode = staticmethod(json.dumps)
     decode = staticmethod(json.loads)
 
-class Invocation(object):
-    __slots__ = ["invoker", "seq"]
-    def __init__(self, invoker, seq):
-        self.invoker = invoker
-        self.seq = seq
+class CompressedJSONSerializer(Serializer):
+    encode = staticmethod(lambda obj: zlib.compress(json.dumps(obj)))
+    decode = staticmethod(lambda blob: json.loads(zlib.decompress(blob)))
 
 
 class Invoker(object):
@@ -149,24 +175,28 @@ class Invoker(object):
         self.transport = transport
         self.serializer = serializer
         self._responses = {}
+    
     def bgcall(self, func, *args):
         blob = self.serializer.encode((func, args))
         seq = self.transport.send(blob)
-        return Invocation(self, seq)
+        return seq
     def call(self, func, *args):
-        resp = self.bgcall(func, *args)
-        return resp.wait()
+        seq = self.bgcall(func, *args)
+        return self.wait_for(seq)
+    
     def _recv(self):
         seq, blob = self.transport.recv()
         try:
             resp, obj = self.serializer.decode(blob)
-        except Exception:
-            pass
+        except Exception as ex:
+            raise ProtocolError("Could not decode blob", ex)
         if resp == RESP_ERROR:
             raise ProtocolError(obj)
+        if resp != RESP_OK and resp != RESP_EXCEPTION:
+            raise ProtocolError("Unknown response code %r" % (resp,))
         self._responses[seq] = (resp, obj)
-        return seq
-    def _wait(self, seq):
+    
+    def wait_for(self, seq):
         while True:
             self._recv()
             if seq not in self._responses:
@@ -176,9 +206,33 @@ class Invoker(object):
                 return obj
             elif resp == RESP_EXCEPTION:
                 raise RemoteException(obj)
-            else:
-                raise ProtocolError("Unknown response code %r" % (resp,))
 
+class Service(object):
+    def __contains__(self, name):
+        return hasattr(self.__class__, "exposed_" + name)
+    def __getitem__(self, name):
+        if not isinstance(name, basestring) or name not in self:
+            raise KeyError(name)
+        return getattr(self, "exposed_" + name)
+
+
+# RPC over RPC
+
+
+
+listener = ListenerSocket(0)
+trns1 = StreamMessageTransport.connect(listener.host, listener.port)
+trns2 = StreamMessageTransport(listener.accept())
+
+
+inv = Invoker(trns1, JSONSerializer)
+disp = Dispatcher(trns2, JSONSerializer, dict(
+        add = lambda x, y: x + y,
+        mul = lambda x, y: x * y,
+    ))
+seq = inv.bgcall("add", 8, 4)
+disp.dispatch()
+print inv.wait_for(seq)
 
 
 """
@@ -190,6 +244,7 @@ class Invoker(object):
 * Options:
     * reconnect
     * timeouts
+    * session layer
 
 """
 
